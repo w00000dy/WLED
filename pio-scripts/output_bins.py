58801d08fedfe1b99bde72b1b2bd241608b23af1
Woody <xaver@holzapfel.net>
2024-01-24 23:50:37 +0100

    output_bins.py optimizations

--- pio-scripts/output_bins.py
+++ pio-scripts/output_bins.py
@@ -4,6 +4,8 @@ import shutil
 import gzip
 
 OUTPUT_DIR = "build_output{}".format(os.path.sep)
+VARIANT = env["PIOENV"]
+FIRMWARE = env.subst("$BUILD_DIR/${PROGNAME}.bin")
 
 def _get_cpp_define_value(env, define):
     define_list = [item[-1] for item in env["CPPDEFINES"] if item[0] == define]
@@ -24,11 +26,10 @@ def _create_dirs(dirs=["firmware", "map"]):
 
 def bin_rename_copy(source, target, env):
     _create_dirs()
-    variant = env["PIOENV"]
-
+    
     # create string with location and file names based on variant
-    map_file = "{}map{}{}.map".format(OUTPUT_DIR, os.path.sep, variant)
-    bin_file = "{}firmware{}{}.bin".format(OUTPUT_DIR, os.path.sep, variant)
+    map_file = "{}map{}{}.map".format(OUTPUT_DIR, os.path.sep, VARIANT)
+    bin_file = "{}firmware{}{}.bin".format(OUTPUT_DIR, os.path.sep, VARIANT)
 
     release_name = _get_cpp_define_value(env, "WLED_RELEASE_NAME")
 
@@ -36,7 +37,7 @@ def bin_rename_copy(source, target, env):
         _create_dirs(["release"])
         version = _get_cpp_define_value(env, "WLED_VERSION")
         release_file = "{}release{}WLED_{}_{}.bin".format(OUTPUT_DIR, os.path.sep, version, release_name)
-        shutil.copy(str(target[0]), release_file)
+        shutil.copy(FIRMWARE, release_file)
 
     # check if new target files exist and remove if necessary
     for f in [map_file, bin_file]:
@@ -44,7 +45,7 @@ def bin_rename_copy(source, target, env):
             os.remove(f)
 
     # copy firmware.bin to firmware/<variant>.bin
-    shutil.copy(str(target[0]), bin_file)
+    shutil.copy(FIRMWARE, bin_file)
 
     # copy firmware.map to map/<variant>.map
     if os.path.isfile("firmware.map"):
@@ -52,11 +53,10 @@ def bin_rename_copy(source, target, env):
 
 def bin_gzip(source, target, env):
     _create_dirs()
-    variant = env["PIOENV"]
 
     # create string with location and file names based on variant
-    bin_file = "{}firmware{}{}.bin".format(OUTPUT_DIR, os.path.sep, variant)
-    gzip_file = "{}firmware{}{}.bin.gz".format(OUTPUT_DIR, os.path.sep, variant)
+    bin_file = "{}firmware{}{}.bin".format(OUTPUT_DIR, os.path.sep, VARIANT)
+    gzip_file = bin_file + ".gz"
 
     # check if new target files exist and remove if necessary
     if os.path.isfile(gzip_file): os.remove(gzip_file)
@@ -66,4 +66,5 @@ def bin_gzip(source, target, env):
         with gzip.open(gzip_file, "wb", compresslevel = 9) as f:
             shutil.copyfileobj(fp, f)
 
-env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", [bin_rename_copy, bin_gzip])
+# Always run regardless of whether the firmware has actually been regenerated or not after a “build”
+env.AddPostAction(FIRMWARE, [bin_rename_copy, bin_gzip])
